d664752bb0eb897e1e9748f9577f8f70c28e1100
Mazhit <kajsar02@gmail.com>
2024-02-16 12:38:17 +0200

    first commit

---
+++ src/components/header/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+backgroundColor: #000;
+padding: 15px 0;
+`
+export const Nav = styled.nav`
+display: flex;
+alightItems: center;
+`
+
+export const HeaderNavList = styled.ul`
+display: flex;
+`
